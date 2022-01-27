c3b85f3225498d9a0192770c16b4dcafddcf00b7
Felipe <felipepasquarodrigues@gmail.com>
2022-01-26 21:41:20 -0300

    TypeScript integrado

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,15 @@
+import Porta from '../components/Porta/Porta'
+import PortaModel from "../model/porta"
+
 export default function Home() {
+
+  const p1 = new PortaModel(1)
+  const p2 = new PortaModel(2)
+
   return (
-    <div>
+    <div style={{ display: 'flex' }}>
+      <Porta porta={p1} />
+      <Porta porta={p2} />
 
     </ div>
   )
